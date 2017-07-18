ddfd4428c0bd775679211620432e1a2601c91eda
Xavier <xavier.beillas@gmail.com>
2017-07-18 16:17:40 +0200

    add route

--- test/test.js
+++ test/test.js
@@ -20,3 +20,13 @@ describe('GET /api/', function() {
       .expect(200, done);
   });
 });
+
+describe('GET /api/status', function() {
+  it('respond with status', function(done) {
+    request(app)
+      .get('/api/status')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
