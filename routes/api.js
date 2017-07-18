51f24e4e373cf6f45ed0715abc917beffe4f70e4
Xavier <xavier.beillas@gmail.com>
2017-07-18 15:57:30 +0200

    check api running

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.json({application: 'exaprint_type'});
 });
 
+router.get('/status', function(req, res) {
+  res.type('json');
+  res.json({status: 'API is running'});
+});
+
 module.exports = router;
