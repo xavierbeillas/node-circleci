b588c6d81e4a170f902f111164cc4701d7778c27
Xavier <xavier.beillas@gmail.com>
2017-07-20 11:55:10 +0200

    unbreak

--- test/test.js
+++ test/test.js
@@ -41,12 +41,3 @@ describe('GET /api/numbers', function() {
   });
 });
 
-describe('GET /api/pool', function() {
-  it('respond with numbers in 200', function(done) {
-    request(app)
-      .get('/api/pool')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-  });
-});
